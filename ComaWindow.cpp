fe2269f00a4ad5b01279b4b7c4bd1921b476b46e
주헌양 <heonyang.ju@projectbombay.com>
2015-06-02 22:16:21 +0900

    ComaWindow 윈도우 출력 부분 구현

--- ComaWindow.cpp
+++ ComaWindow.cpp
@@ -2,11 +2,93 @@
 #include "ComaWindow.h"
 using namespace Coma2D;
 
-ComaWindow::ComaWindow()
+ComaWindow* comaWindow;
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	return comaWindow->messageProc(hWnd, uMsg, wParam, lParam);
+}
+
+ComaWindow::ComaWindow(HINSTANCE hInstance)
+	:hInstance(0), hWnd(0)
+{
+	this->hInstance = hInstance;
+	comaWindow = this;
 }
 
 
 ComaWindow::~ComaWindow()
 {
+}
+
+
+bool ComaWindow::createWindow()
+{
+	const char* className = "Coma2DWindow";
+	WNDCLASS windowClass;
+	
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = MainWndProc;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = hInstance;
+	windowClass.hIcon = LoadIcon(0, IDI_APPLICATION);
+	windowClass.hCursor = LoadCursor(0, IDC_ARROW);
+	windowClass.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+	windowClass.lpszMenuName = 0;
+	windowClass.lpszClassName = className;
+
+	if (!RegisterClass(&windowClass))
+		return false;
+
+	RECT rect = { 0, 0, 800, 480 };
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+	
+	hWnd = CreateWindow(
+		className,
+		"Coma2DWindow",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 
+		rect.right - rect.left, rect.bottom - rect.top,
+		0, 0, hInstance, 0);
+	
+	if (!hWnd)
+		return false;
+	
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	
+	return true;
+}
+
+bool ComaWindow::run()
+{
+	if (!hWnd)
+		return false;
+	MSG msg = { 0 };
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			
+		}
+	}
+	return true;
+}
+
+LRESULT ComaWindow::messageProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
--- ComaWindow.h
+++ ComaWindow.h
@@ -1,14 +1,60 @@
 #pragma once
 #include "EventDispatcher.h"
-
+#include "ComaRenderer.h"
 namespace Coma2D
 {
 	class ComaWindow :
 		public EventDispatcher
 	{
 	public:
-		ComaWindow();
+		ComaWindow(HINSTANCE hInstance);
 		virtual ~ComaWindow();
+
+		bool	createWindow();
+		bool	run();
+		
+		LRESULT messageProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	private:
+		HINSTANCE	hInstance;
+		HWND		hWnd;
+		
+
+		/*
+		//Getter
+		HINSTANCE		getInstance()	{return hInstance;}
+		HWND			getWindow()		{return hWnd;}
+		ComaRenderer*	getRenderer()	{return renderer;}
+		bool			isRunning()		{return isRunning;}
+
+
+		//Setter
+		bool			setRenderer(ComaRenderer* renderer);
+		bool			setSize(int width, int height);
+		bool			setMinimumSize(int width, int height);
+		bool			setMaximumSize(int width, int height);
+		void			setTitle(const char* title);
+		
+
+	private:
+		//Windows Essensials
+		HINSTANCE		hInstance;
+		HWND			hWnd;
+		ComaRenderer*	renderer;
+		
+		
+		//Windows Attributes
+		POINT			clientPosition;
+		RECT			clientRect;
+		RECT			minimumClientSize;
+		RECT			maximumClientSize;
+		bool			resizable;
+		const char*		title;
+
+		//Windows Status
+		
+		bool			isAvailable;
+		bool			isResizing;
+		bool			isRunning; */
 	};
 }
 
