a1fe3e19818e4ffc5439d0f94d017fbbb36ad700
주헌양 <heonyang.ju@projectbombay.com>
2015-06-04 11:56:19 +0900

    ComaRenderer 기초 렌더링 기능 추가

--- ComaRenderer.cpp
+++ ComaRenderer.cpp
@@ -6,6 +6,7 @@ using namespace Coma2D;
 ComaRenderer::ComaRenderer()
 	:factory(NULL), renderTarget(NULL), initialized(false)
 {
+	backgroundColor = D2D1::ColorF(0.0f, 0.0f, 0.0f, 1.0f);
 }
 
 ComaRenderer::~ComaRenderer()
@@ -17,14 +18,13 @@ bool ComaRenderer::initRenderer(HWND hWnd)
 	HRESULT res = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &factory);
 	if (res != S_OK)
 		return false;
-	RECT rect;
-	GetClientRect(hWnd, &rect);
-	resizeRenderTarget();
+	createRenderTarget(hWnd);
+
 	initialized = true;
 	this->hWnd = hWnd;
 	return true;
 }
-void ComaRenderer::resizeRenderTarget()
+void ComaRenderer::createRenderTarget(HWND hWnd)
 {
 	if(renderTarget) renderTarget->Release();
 	RECT rect;
@@ -34,4 +34,14 @@ void ComaRenderer::resizeRenderTarget()
 		D2D1::HwndRenderTargetProperties(hWnd, D2D1::SizeU(rect.right, rect.bottom)),
 		&renderTarget
 		);
+}
+void ComaRenderer::resizeWindow()
+{
+	createRenderTarget(hWnd);
+}
+void ComaRenderer::render()
+{
+	renderTarget->BeginDraw();
+	renderTarget->Clear(backgroundColor);
+	renderTarget->EndDraw();
 }
--- ComaRenderer.h
+++ ComaRenderer.h
@@ -8,16 +8,31 @@ namespace Coma2D
 	public:
 		ComaRenderer();
 		virtual ~ComaRenderer();
+
 		bool initRenderer(HWND hWnd);
+		void render();
+
+		//Status Getter
 		bool isInitialized(){ return initialized; }
 
-		void resizeRenderTarget();
+		//Status Setter
+		void setBackgroundColor(float r, float g, float b, float a = 1.0f){ setBackgroundColor(D2D1::ColorF(r, g, b, a)); };
+		void setBackgroundColor(D2D1::ColorF color) { backgroundColor = color; }
+
+		//Status Changer
+		void resizeWindow();
+
 	protected:
+		//Renderer Essencials
 		ID2D1Factory* factory;
 		ID2D1HwndRenderTarget* renderTarget;
 	private:
+		//Renderer Status
 		HWND hWnd;
-
 		bool initialized;
+		D2D1_COLOR_F backgroundColor;
+
+	private:
+		void createRenderTarget(HWND hWnd);
 	};
 }
