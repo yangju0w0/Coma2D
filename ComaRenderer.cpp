32e9aab82048f70758a3752ee8fbc66b48ce7732
주헌양 <heonyang.ju@projectbombay.com>
2015-06-04 11:32:08 +0900

    렌더러 초기화 및 리사이징 추가

--- ComaRenderer.cpp
+++ ComaRenderer.cpp
@@ -4,10 +4,34 @@ using namespace Coma2D;
 
 
 ComaRenderer::ComaRenderer()
+	:factory(NULL), renderTarget(NULL), initialized(false)
 {
 }
 
-
 ComaRenderer::~ComaRenderer()
 {
+}
+
+bool ComaRenderer::initRenderer(HWND hWnd)
+{
+	HRESULT res = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &factory);
+	if (res != S_OK)
+		return false;
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	resizeRenderTarget();
+	initialized = true;
+	this->hWnd = hWnd;
+	return true;
+}
+void ComaRenderer::resizeRenderTarget()
+{
+	if(renderTarget) renderTarget->Release();
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	factory->CreateHwndRenderTarget(
+		D2D1::RenderTargetProperties(),
+		D2D1::HwndRenderTargetProperties(hWnd, D2D1::SizeU(rect.right, rect.bottom)),
+		&renderTarget
+		);
 }
--- ComaRenderer.h
+++ ComaRenderer.h
@@ -8,5 +8,16 @@ namespace Coma2D
 	public:
 		ComaRenderer();
 		virtual ~ComaRenderer();
+		bool initRenderer(HWND hWnd);
+		bool isInitialized(){ return initialized; }
+
+		void resizeRenderTarget();
+	protected:
+		ID2D1Factory* factory;
+		ID2D1HwndRenderTarget* renderTarget;
+	private:
+		HWND hWnd;
+
+		bool initialized;
 	};
 }
