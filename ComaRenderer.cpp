e3f3e3cdbebd0be24e7f64ee5afbc69abcdbfe3a
주헌양 <heonyang.ju@projectbombay.com>
2015-06-10 20:40:56 +0900

    ComaRenderer 기본 기능 작성

--- ComaRenderer.cpp
+++ ComaRenderer.cpp
@@ -4,10 +4,79 @@ using namespace coma2d;
 
 
 ComaRenderer::ComaRenderer()
+	:initialized(false), running(false)
 {
+	backgroundColor = D2D1::ColorF(0.0f, 0.0f, 0.0f);
 }
 
 
 ComaRenderer::~ComaRenderer()
 {
 }
+
+bool ComaRenderer::initRenderer(HWND hWnd)
+{
+	if (initialized)
+		return false;
+
+	if (D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &factory) != S_OK)
+		return false;
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+
+	if (factory->CreateHwndRenderTarget(
+		D2D1::RenderTargetProperties(),
+		D2D1::HwndRenderTargetProperties(hWnd, D2D1::SizeU(rect.right, rect.bottom)),
+		&renderTarget
+		) != S_OK)
+		return false;
+
+	targetWindow = hWnd;
+	initialized = true;
+	return true;
+}
+
+bool ComaRenderer::run()
+{
+	if (!initialized || running)
+		return false;
+
+	running = true;
+	return true;
+}
+bool ComaRenderer::pause()
+{
+	if (!initialized || !running)
+		return false;
+
+	running = false;
+	return true;
+}
+bool ComaRenderer::update()
+{
+	if (!initialized || !running)
+		return false;
+
+	renderTarget->BeginDraw();
+	renderTarget->Clear(backgroundColor);
+	renderTarget->EndDraw();
+
+	return true;
+}
+bool ComaRenderer::resetSize()
+{
+	renderTarget->Release();
+
+	RECT rect;
+	GetClientRect(targetWindow, &rect);
+
+	if (factory->CreateHwndRenderTarget(
+		D2D1::RenderTargetProperties(),
+		D2D1::HwndRenderTargetProperties(targetWindow, D2D1::SizeU(rect.right, rect.bottom)),
+		&renderTarget
+		) != S_OK)
+		return false;
+
+	return true;
+}
+
--- ComaRenderer.h
+++ ComaRenderer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "EventDispatcher.h"
+#include <d2d1.h>
 namespace coma2d
 {
 	class ComaRenderer :
@@ -8,5 +9,36 @@ namespace coma2d
 	public:
 		ComaRenderer();
 		virtual ~ComaRenderer();
+		
+		//Renderer Controls
+		bool initRenderer(HWND hWnd);
+		bool run();
+		bool pause();
+
+		bool update();
+		bool resetSize();
+
+	public:
+		//Renderer Status Getter
+		bool isInitialized(){ return initialized; }
+		bool isRunning(){ return running; }
+
+		//Renderer Attributes Setter
+		void setBackgroundColor(D2D1_COLOR_F color) { backgroundColor = color; }
+		void setBackgroundColor(float r, float g, float b) { backgroundColor = D2D1::ColorF(r, g, b); }
+
+	private:
+		//Renderer Status
+		bool initialized;
+		bool running;
+
+	private:
+		//Renderer Essencials
+		ID2D1Factory* factory;
+		ID2D1HwndRenderTarget* renderTarget;
+		HWND targetWindow;
+
+		//Renderer Attributes
+		D2D1_COLOR_F backgroundColor;
 	};
 }
