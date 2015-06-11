edd5afd186ef26041f4af32df7392bf7b0685965
주헌양 <heonyang.ju@projectbombay.com>
2015-06-11 21:45:35 +0900

    RendererEvent 개발

--- RendererEvent.cpp
+++ RendererEvent.cpp
@@ -2,9 +2,19 @@
 #include "RendererEvent.h"
 using namespace coma2d;
 
-RendererEvent::RendererEvent(std::string type)
+const std::string RendererEvent::UPDATE					= "renderer_update";
+const std::string RendererEvent::RENDER					= "renderer_render";
+const std::string RendererEvent::PAUSED					= "renderer_renderPaused";
+const std::string RendererEvent::RUN					= "renderer_renderRun";
+const std::string RendererEvent::UNLOAD_RESOURCES_REQ	= "renderer_unloadResourceRequest";
+const std::string RendererEvent::LOAD_RESOURCE_REQ		= "renderer_loadResourceRequest";
+const std::string RendererEvent::RENDER_TARGET_CREATED	= "renderer_renderTargetCreated";
+const std::string RendererEvent::RENDER_TARGET_RELEASED = "renderer_renderTargetReleased";
+
+RendererEvent::RendererEvent(std::string type, ComaRenderer* target)
 	:Event(type)
 {
+	targetRenderer = target;
 }
 
 
