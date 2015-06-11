b9d4d33126133f3700775840b13bb5b14fb27cb5
주헌양 <heonyang.ju@projectbombay.com>
2015-06-11 17:42:28 +0900

    RenderTimer 클래스 개발

---
+++ RenderTimer.cpp
@@ -0,0 +1,79 @@
+#include "stdafx.h"
+#include "RenderTimer.h"
+
+
+RenderTimer::RenderTimer()
+	:baseTime(0), pausedTime(0), stopTime(0), prevTime(0), currentTime(0), secondsPerCount(0), deltaTime(0), running(false)
+{
+	int freq;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+	secondsPerCount = 1.0 / freq;
+}
+
+
+RenderTimer::~RenderTimer()
+{
+}
+
+void RenderTimer::reset()
+{
+	running = false;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	baseTime = currentTime;
+	stopTime = currentTime;
+	prevTime = currentTime;
+	pausedTime = 0;
+}
+
+void RenderTimer::start()
+{
+	if (running)
+		return;
+
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	pausedTime += currentTime - stopTime;
+
+	prevTime = currentTime;
+
+	running = true;
+}
+
+void RenderTimer::stop()
+{
+	if (!running)
+		return;
+	
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	stopTime = currentTime;
+	running = false;
+}
+
+void RenderTimer::tick()
+{
+	if (!running)
+		return;
+
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	deltaTime = (currentTime - prevTime) * secondsPerCount;
+	prevTime = currentTime;
+}
+
+double RenderTimer::getTotalTime()
+{
+	return (currentTime - baseTime)*secondsPerCount;
+}
+
+double RenderTimer::getRunningTime()
+{
+	return (currentTime - baseTime - pausedTime)*secondsPerCount;
+}
+
+double RenderTimer::getPausedTime()
+{
+	return pausedTime*secondsPerCount;
+}
+
+double RenderTimer::getDeltaTime()
+{
+	return deltaTime;
+}
