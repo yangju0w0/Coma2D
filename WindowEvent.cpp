b7169797e019a05602a560af973f3adee1b23cf8
주헌양 <heonyang.ju@projectbombay.com>
2015-06-11 21:29:37 +0900

    윈도우 이벤트 형식 수정

--- WindowEvent.cpp
+++ WindowEvent.cpp
@@ -2,26 +2,26 @@
 #include "WindowEvent.h"
 using namespace coma2d;
 
-const std::string WindowEvent::CREATED			= "created";
-const std::string WindowEvent::DESTROY			= "destroy";
+const std::string WindowEvent::CREATED			= "window_created";
+const std::string WindowEvent::DESTROY			= "window_destroy";
 
-const std::string WindowEvent::ACTIVATED		= "activated";
-const std::string WindowEvent::INACTIVATED		= "inactivated";
+const std::string WindowEvent::ACTIVATED		= "window_activated";
+const std::string WindowEvent::INACTIVATED		= "window_inactivated";
 
-const std::string WindowEvent::MINIMIZED		= "minimized";
-const std::string WindowEvent::MAXIMIZED		= "maximized";
-const std::string WindowEvent::RESTORED			= "restored";
-const std::string WindowEvent::ENTER_FULLSCREEN	= "enterFullscreen";
-const std::string WindowEvent::EXIT_FULLSCREEN	= "exitFullscreen";
+const std::string WindowEvent::MINIMIZED		= "window_minimized";
+const std::string WindowEvent::MAXIMIZED		= "window_maximized";
+const std::string WindowEvent::RESTORED			= "window_restored";
+const std::string WindowEvent::ENTER_FULLSCREEN	= "window_enterFullscreen";
+const std::string WindowEvent::EXIT_FULLSCREEN	= "window_exitFullscreen";
 
-const std::string WindowEvent::ENTER_RESIZEMOVE	= "enterResizeMove";
-const std::string WindowEvent::EXIT_RESIZEMOVE	= "exitResizeMove";
-const std::string WindowEvent::RESIZE			= "resize";
-const std::string WindowEvent::RESIZING			= "resizing";
-const std::string WindowEvent::MOVE				= "move";
-const std::string WindowEvent::MOVING			= "moving";
+const std::string WindowEvent::ENTER_RESIZEMOVE	= "window_enterResizeMove";
+const std::string WindowEvent::EXIT_RESIZEMOVE	= "window_exitResizeMove";
+const std::string WindowEvent::RESIZE			= "window_resize";
+const std::string WindowEvent::RESIZING			= "window_resizing";
+const std::string WindowEvent::MOVE				= "window_move";
+const std::string WindowEvent::MOVING			= "window_moving";
 
-const std::string WindowEvent::UPDATE			= "update";
+const std::string WindowEvent::UPDATE			= "window_update";
 
 WindowEvent::WindowEvent(std::string type, ComaWindow* target, WPARAM wParam, LPARAM lParam)
 	:Event(type)
