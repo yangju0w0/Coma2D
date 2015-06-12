7ddb3c11e83c650ffabfa8d20fec3c212492bd4e
주헌양 <heonyang.ju@projectbombay.com>
2015-06-12 09:25:25 +0900

    ComaDevice 기본 기능 작성

--- ComaDevice.cpp
+++ ComaDevice.cpp
@@ -1,12 +1,159 @@
 #include "stdafx.h"
 #include "ComaDevice.h"
+#include "WindowEvent.h"
+#include "RendererEvent.h"
+
 using namespace coma2d;
 
 ComaDevice::ComaDevice()
+	:running(false), initialized(false)
 {
 }
 
+ComaDevice::ComaDevice(ComaWindow* window, ComaRenderer* renderer)
+{
+	ComaDevice();
+	setWindowRenderer(window, renderer);
+}
 
 ComaDevice::~ComaDevice()
 {
 }
+
+bool ComaDevice::setWindow(ComaWindow* window)
+{
+	if (initialized)
+		return false;
+	this->window = window;
+	return true;
+}
+bool ComaDevice::setRenderer(ComaRenderer* renderer)
+{
+	if (initialized)
+		return false;
+	this->renderer = renderer;
+	return true;
+}
+bool ComaDevice::setWindowRenderer(ComaWindow* window, ComaRenderer* renderer)
+{
+	if (!setWindow(window))
+		return false;
+	if (!setRenderer(renderer))
+		return false;
+	return true;
+}
+
+bool ComaDevice::initDevice()
+{
+	if (initialized)
+		return false;
+	if (!initWindow())
+		return false;
+	if (!initRenderer())
+		return false;
+	initialized = true;
+	return true;
+}
+bool ComaDevice::initDevice(HINSTANCE hInstance)
+{
+	if (initialized)
+		return false;
+	if (!initWindow(hInstance))
+		return false;
+	if (!initRenderer())
+		return false;
+	initialized = true;
+	return true;
+}
+
+bool ComaDevice::run()
+{
+	if (running || !initialized || !window || !renderer)
+		return false;
+	running = true;
+
+	if (!renderer->isRunning())
+		renderer->run();
+
+	if (!window->isRunning())
+		window->run();
+
+	return true;
+}
+
+bool ComaDevice::initWindow()
+{
+	if (!window)
+		return false;
+	if (!window->isCreated())
+		if (!window->createWindow())
+			return false;
+
+	window->setEventListener(WindowEvent::DESTROY,			BIND(ComaDevice::windowDestroyListener			));
+	window->setEventListener(WindowEvent::MINIMIZED,		BIND(ComaDevice::windowMinimizeListener			));
+	window->setEventListener(WindowEvent::RESTORED,			BIND(ComaDevice::windowRestoreListener			));
+	window->setEventListener(WindowEvent::RESIZE,			BIND(ComaDevice::windowResizeListener			));
+	window->setEventListener(WindowEvent::ENTER_RESIZEMOVE, BIND(ComaDevice::windowEnterResizeMoveListener	));
+	window->setEventListener(WindowEvent::EXIT_RESIZEMOVE,	BIND(ComaDevice::windowExitResizeMoveListener	));
+	window->setEventListener(WindowEvent::UPDATE,			BIND(ComaDevice::windowUpdateListener			));
+	return true;
+}
+bool ComaDevice::initWindow(HINSTANCE hInstance)
+{
+	if (window)
+		return initWindow();
+
+	window = new ComaWindow(hInstance);
+	return initWindow();
+}
+
+bool ComaDevice::initRenderer()
+{
+	if (!window)
+		return false;
+
+	if (!renderer)
+		renderer = new ComaRenderer();
+
+	if (!renderer->isInitialized())
+		if (!renderer->initRenderer(window->getWindow()))
+			return false;
+
+	renderer->setEventListener(RendererEvent::UPDATE, BIND(ComaDevice::rendererUpdateListener));
+	renderer->setEventListener(RendererEvent::RENDER, BIND(ComaDevice::rendererRenderListener));
+		
+	return true;
+}
+
+void ComaDevice::windowDestroyListener(Event* event)
+{
+	
+}
+void ComaDevice::windowMinimizeListener(Event* event)
+{
+	renderer->pause();
+}
+void ComaDevice::windowRestoreListener(Event* event)
+{
+	renderer->run();
+}
+void ComaDevice::windowEnterResizeMoveListener(Event* event)
+{
+	renderer->pause();
+}
+void ComaDevice::windowExitResizeMoveListener(Event* event)
+{
+	renderer->run();
+}
+void ComaDevice::windowUpdateListener(Event* event)
+{
+	renderer->update();
+}
+void ComaDevice::windowResizeListener(Event* event)
+{
+	if (!window->isResizing() && !window->isMinimized())
+		renderer->resetSize();
+}
+
+void ComaDevice::rendererUpdateListener(Event* event){}
+void ComaDevice::rendererRenderListener(Event* event){}
--- ComaDevice.h
+++ ComaDevice.h
@@ -1,10 +1,54 @@
 #pragma once
+#include "ComaWindow.h"
+#include "ComaRenderer.h"
 namespace coma2d
 {
 	class ComaDevice
 	{
 	public:
 		ComaDevice();
+		ComaDevice(ComaWindow* window, ComaRenderer* renderer);
 		virtual ~ComaDevice();
+
+		//Device Setter
+		bool setWindow(ComaWindow* window);
+		bool setRenderer(ComaRenderer* renderer);
+		bool setWindowRenderer(ComaWindow* window, ComaRenderer* renderer);
+
+		//Device Control
+		bool initDevice();
+		bool initDevice(HINSTANCE hInstance);
+		bool run();
+
+		//Device Essencials Getter
+		ComaWindow* getWindow(){ return window; }
+		ComaRenderer* getRenderer(){ return renderer; }
+
+		//Device Status Getter
+		bool isRunning(){ return running; }
+		bool isInitialized(){ return initialized; }
+
+		void windowDestroyListener(Event* event);
+		void windowMinimizeListener(Event* event);
+		void windowRestoreListener(Event* event);
+		void windowResizeListener(Event* event);
+		void windowEnterResizeMoveListener(Event* event);
+		void windowExitResizeMoveListener(Event* event);
+		void windowUpdateListener(Event* event);
+		void rendererUpdateListener(Event* event);
+		void rendererRenderListener(Event* event);
+
+	private:
+		bool initWindow();
+		bool initWindow(HINSTANCE hInstance);
+		bool initRenderer();
+	private:
+		//Device Essencials
+		ComaWindow* window;
+		ComaRenderer* renderer;
+
+		//Device Status
+		bool running;
+		bool initialized;
 	};
 }
