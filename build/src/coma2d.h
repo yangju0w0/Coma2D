ca1fdbd4954bd4df5e4d6d76f4f4198ac8f865bf
주헌양 <heonyang.ju@projectbombay.com>
2015-06-29 14:34:42 +0900

    편의 클래스 및 기능 추가

---
+++ build/src/coma2d.h
@@ -0,0 +1,16 @@
+#include "comaUtil.h"
+#include "ComaDevice.h"
+#include "ComaRenderer.h"
+#include "ComaRenderer.h"
+
+#include "Resource.h"
+#include "Bitmap.h"
+
+#include "Manager.h"
+#include "ResourceManager.h"
+
+#include "Event.h"
+#include "EventDispatcher.h"
+#include "RendererEvent.h"
+#include "ResourceManagerEvent.h"
+#include "WindowEvent.h"
--- build/src/comaUtil.h
+++ build/src/comaUtil.h
@@ -49,8 +49,24 @@
 * fileinfo	매크로 및 유틸리티 집합 파일
 * author	주헌양 (Heonyang Ju)
 */
+#include <d2d1.h>
+#include <math.h>
 
 #define COMA_NAMESPACE coma2d
 #define COMA_NS_BEGIN namespace COMA_NAMESPACE {
 #define COMA_USING_NS using namespace COMA_NAMESPACE;
 #define COMA_END }
+
+#define PI 3.1415926535
+#define RADIAN_TO_DEGREE(x)((x)*(180.0/PI));
+#define DEGREE_TO_RADIAN(x)((x)*(PI/180.0));
+
+typedef D2D_POINT_2U PointU;
+typedef D2D_POINT_2F Point;
+typedef D2D_RECT_F Rect;
+typedef D2D_RECT_U RectU;
+typedef D2D_SIZE_F Size;
+typedef D2D_SIZE_U SizeU;
+typedef D2D_COLOR_F Color;
+typedef D2D_MATRIX_3X2_F Matrix3x2;
+typedef UINT64 TAG;
